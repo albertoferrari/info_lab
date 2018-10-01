d3c18b4f1a48b98ad2076f58cbbe51341a07f1cf
Alberto Ferrari <alberto.ferrari@unipr.it>
2018-10-01 08:20:40 +0200

    Add files via upload

--- codice_lezioni/sl03_04_cicli_annidati.py
+++ codice_lezioni/sl03_04_cicli_annidati.py
@@ -3,12 +3,12 @@
  @license This software is free - http://www.gnu.org/licenses/gpl.html
  
  cicli annidati
+ (per formattazione output -> https://pyformat.info)
 '''
 
 for riga in range(1, 13):
     for colonna in range(1, 13):
         val = riga * colonna
-        print(f"{val:3}", end=" ")  # val represented as text
-                                    # with at least 3 chars
+        print(f"{val:3}", end=" ")  # val testo di almeno 3 caratteri
     print()
 
