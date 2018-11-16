c9bfabd08058f6903d366fb41db1cce9e4ac9626
Alberto Ferrari <alberto.ferrari@unipr.it>
2018-11-16 11:19:47 +0100

    Add files via upload

---
+++ esempi_python_cpp/bounce Python and Cpp/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import cppyy
+cppyy.include("actor.cpp")
+cppyy.include("bounce.cpp")
+from cppyy.gbl import Arena, Ball, Ghost, Turtle
+
+import g2d
+
+arena = Arena(320, 240)
+b1 = Ball(arena, 40, 80)
+b2 = Ball(arena, 80, 40)
+g = Ghost(arena, 120, 80)
+turtle = Turtle(arena, 80, 80)
+sprites = g2d.load_image("sprites.png")
+
+def update():
+    arena.move_all()  # Game logic
+
+    g2d.fill_canvas((255, 255, 255))
+    for a in arena.actors():
+        g2d.draw_image_clip(sprites, a.position(), a.symbol())
+
+def keydown(code):
+    if code == "ArrowUp":
+        turtle.go_up()
+    elif code == "ArrowDown":
+        turtle.go_down()
+    elif code == "ArrowLeft":
+        turtle.go_left()
+    elif code == "ArrowRight":
+        turtle.go_right()
+
+def keyup(code):
+    turtle.stay()
+
+def main():
+    g2d.init_canvas(arena.size())
+    g2d.handle_keyboard(keydown, keyup)
+    g2d.main_loop(update, 1000 // 30)
+
+main()
