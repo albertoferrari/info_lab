8d16e06988d8358ede9e42b4abd25ef681118b40
Alberto Ferrari <alberto.ferrari@unipr.it>
2019-10-25 18:23:27 +0200

    Add files via upload

---
+++ codice_lezioni/board game/lightsout_gui.py
@@ -0,0 +1,56 @@
+'''
+ LightsOut
+ https://en.wikipedia.org/wiki/Lights_Out_(game)
+ http://www.millstone.demon.co.uk/games/lightsout/lightsout.htm
+'''
+from boardgame import BoardGame
+from boardgamegui import BoardGameGui, gui_play
+from random import randrange
+
+class LightsOut(BoardGame):
+
+	def __init__(self, side=5, level=4):
+		self._cols, self._rows = side, side
+		self._board = [[False for x in range(side)] for y in range(side)]
+		for _ in range(level):
+			self.play_at(randrange(side), randrange(side))
+
+	def cols(self) -> int:
+		return self._cols
+
+	def rows(self) -> int:
+		return self._rows
+
+	def finished(self) -> bool:
+		for y in range(self._rows):
+			for x in range(self._cols):
+				if self._board[y][x]:
+					return False
+		return True
+
+	def play_at(self, x: int, y: int):
+		'''Place (or remove) a light at cell (x, y)'''
+		if 0 <= x < self._cols and 0 <= y < self._rows:
+			for dx, dy in ((0, 0), (0, -1), (1, 0),
+						   (0, 1), (-1, 0)):
+				x1, y1 = x + dx, y + dy
+				if 0 <= x1 < self._cols and 0 <= y1 < self._rows:
+					self._board[y1][x1] = not self._board[y1][x1]
+
+	def flag_at(self, x: int, y: int):
+		pass
+
+	def value_at(self, x: int, y: int) -> str:
+		if (0 <= x < self._cols and 0 <= y < self._rows and self._board[y][x]):
+			return '@'
+		return '-'
+
+	def message(self) -> str:
+		return "Puzzle solved!"
+
+
+def main():
+	game = LightsOut()
+	gui_play(game)
+
+main()
